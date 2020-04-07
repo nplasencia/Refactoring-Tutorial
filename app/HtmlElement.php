dc54270dc8408b3fe8828ba5e89a2bdd2a8a0cb6
Nauzet Plasencia <nauzet.plasencia.cruz@gmail.com>
2020-04-07 09:39:30 +0000

    Lesson 3 - Structured code created. After this we will refactor it

--- app/HtmlElement.php
+++ app/HtmlElement.php
@@ -5,8 +5,51 @@ namespace App;
 
 class HtmlElement
 {
+
+    private $name;
+    private $content;
+    /**
+     * @var array
+     */
+    private $attributes;
+
+    public function __construct(string $name, array $attributes = null, $content = null)
+    {
+        $this->name = $name;
+        $this->attributes = $attributes;
+        $this->content = $content;
+    }
+
     public function render()
     {
-        return 'HTML Element Here';
+        if (!empty($this->attributes)) {
+            $htmlAttributes = '';
+
+            foreach ($this->attributes as $key => $value) {
+                if (is_numeric($key)) {
+                    //For example: 'required'
+                    $htmlAttributes .= " $value";
+                } else {
+                    $htmlAttributes .= " $key=\"".htmlentities($value, ENT_QUOTES, 'UTF-8')."\"";
+                }
+            }
+
+            $result = "<{$this->name}$htmlAttributes>";
+        } else {
+            $result = "<{$this->name}>";
+        }
+
+        // If the element is void, we have to return
+        if (in_array($this->name, ['br', 'hr', 'img', 'input', 'meta'])) {
+            return $result;
+        }
+
+        //Print element content
+        $result .= htmlentities($this->content, ENT_QUOTES, 'UTF-8');
+
+        // Close the element
+        $result .= "</{$this->name}>";
+
+        return $result;
     }
 }
--- public/index.php
+++ public/index.php
@@ -4,6 +4,30 @@ require '../vendor/autoload.php';
 
 use App\HtmlElement;
 
-$htmlElement = new HtmlElement();
+$htmlElement = new HtmlElement('p', [],'This is the content');
+echo htmlentities($htmlElement->render(), ENT_QUOTES, 'UTF-8');
 
-echo $htmlElement->render();
+echo "<br><br>";
+
+$htmlElement = new HtmlElement('p', ['id' => 'my_paragraph'],'This is the content with attributes');
+echo htmlentities($htmlElement->render(), ENT_QUOTES, 'UTF-8');
+
+echo "<br><br>";
+
+$htmlElement = new HtmlElement('p', ['id' => 'my_paragraph', 'class' => 'paragraph'],'This is the content with attributes');
+echo htmlentities($htmlElement->render(), ENT_QUOTES, 'UTF-8');
+
+echo "<br><br>";
+
+$htmlElement = new HtmlElement('img', ['src' => 'img/auret.png']);
+echo htmlentities($htmlElement->render(), ENT_QUOTES, 'UTF-8');
+
+echo "<br><br>";
+
+$htmlElement = new HtmlElement('img', ['src' => 'img/auret.png', 'title' => '"Refactoring" course']);
+echo htmlentities($htmlElement->render(), ENT_QUOTES, 'UTF-8');
+
+echo "<br><br>";
+
+$htmlElement = new HtmlElement('input', ['required']);
+echo htmlentities($htmlElement->render(), ENT_QUOTES, 'UTF-8');
